2749bee368e6c74cb1c30ede515223082cb814ff
TomDeeterink1 <deeterinktom@gmail.com>
2024-04-22 23:38:00 +0200

    index full page push

--- public/animations.js
+++ public/animations.js
@@ -1,3 +1,17 @@
+// // popup animation
+
+// const timeliner = gsap.timeline()
+//  timeliner.from('.popup', {duration: 0.8 , x: '200vw'});
+
+//  const popupbutton = document.getElementById('popbutton');
+
+//  popupbutton.addEventListener('click', () =>{
+//     timeliner.reverse()
+//  })
+
+
+
+
 //    carousel animation
    gsap.from('.card', {duration: 1.06, x: '100vw', ease: "sine.out"});
 
@@ -5,3 +19,4 @@
 const timeline = gsap.timeline()
     timeline.from('.detail--text-image' , { duration: 1.06, x: '-110vw'})
     timeline.from('.fav-carousel' , {duration:0.9 , x:'-50vw' ,ease: "sine.out" })
+
