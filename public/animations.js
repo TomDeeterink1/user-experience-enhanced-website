d38848f15edfb88c71c1c25638a3ca2c32c9bf22
TomDeeterink1 <deeterinktom@gmail.com>
2024-05-08 12:53:57 +0200

    push

--- public/animations.js
+++ public/animations.js
@@ -1,15 +1,15 @@
 
-//    carousel animation
-   gsap.from('.card', {duration: 1.01, y: '100vw'});
+// //    carousel animation
+//    gsap.from('.card', {duration: 1.01, y: '100vw'});
 
-// detail page animation
-const timeline = gsap.timeline()
-    timeline.from('.detail--text-image' , { duration: 1.06, x: '-110vw'})
-    timeline.from('.fav-carousel' , {duration:0.9 , x:'-50vw' ,ease: "sine.out" })
+// // detail page animation
+// const timeline = gsap.timeline()
+//     timeline.from('.detail--text-image' , { duration: 1.06, x: '-110vw'})
+//     timeline.from('.fav-carousel' , {duration:0.9 , x:'-50vw' ,ease: "sine.out" })
 
-// overview page
-const timeline2 = gsap.timeline()
-   timeline2.from('.family-overview--intro' , { duration: 1, x: '-110vw'}) 
-   timeline2.from('.users' , {duration: 0.5 , x: '-100vw' ,ease: "sine.out"})
-   timeline2.from('.stats' , {duration: 0.5 , y: '100vh'})
+// // overview page
+// const timeline2 = gsap.timeline()
+//    timeline2.from('.family-overview--intro' , { duration: 1, x: '-110vw'}) 
+//    timeline2.from('.users' , {duration: 0.5 , x: '-100vw' ,ease: "sine.out"})
+//    timeline2.from('.stats' , {duration: 0.5 , y: '100vh'})
 
